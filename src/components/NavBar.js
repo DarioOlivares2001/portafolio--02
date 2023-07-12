dc2b49039e3fedc1e625466ccd294d59df6437d2
Dario Olivares <olivaresdario2001@gmail.com>
2023-07-12 16:40:29 -0400

    nuevas modificaciones

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -52,7 +52,7 @@ const NavBar = () => {
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
-                <a href="#"><img src={navIcon1} alt="" /></a>
+                <a href="https://www.linkedin.com/in/dario-olivares-32872016a"><img src={navIcon1} alt="" /></a>
                 <a href="#"><img src={navIcon2} alt="" /></a>
                 <a href="#"><img src={navIcon3} alt="" /></a>
               </div>
